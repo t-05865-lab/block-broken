9377783222addff24aa06b03254b8412309e1dd4
t-05865-lab <t-05865@koto.ed.jp>
2026-02-16 14:11:34 +0900

    Update game.js

--- game.js
+++ game.js
@@ -41,10 +41,11 @@ document.addEventListener("keyup", e => {
 
 // --- クリックでゲーム開始 ---
 canvas.addEventListener("click", () => {
-  if (gameState === "title") {
+  if (gameState === "title"|| gameState === "gameover") {
     level = 1;
     gameState = "playing";
     resetGame();
+    draw();
   }
 });
 
